8591453eef10799f5fb1b20151813b3fe03fe68b
JonahJL <55671274+JonahJL@users.noreply.github.com>
2026-03-12 14:19:32 -0400

    Set Vite base to '/' for user GitHub Pages site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jonahleinwand.github.io/',
+  // For a user site hosted at https://<username>.github.io/, use root base '/'
+  // If this were a project site (github.io/<repo>), set base to '/<repo>/'
+  base: '/',
   plugins: [react()],
 })
